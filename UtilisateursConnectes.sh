5249a7f7aeccd0fc5e54a42380579d2507074431
Antoine Letourneur <ant984@free.fr>
2016-05-20 15:02:22 +0200

    maj scripts bash pour format json

--- UtilisateursConnectes.sh
+++ UtilisateursConnectes.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-UtilisateursNb=`users | wc -w`
+UtilisateursNb=`users | xargs -n1 | uniq | wc -w`
 
 echo "Nombre d'utilisateurs connectés : $UtilisateursNb"
+
+if [ ! -f UtilisateursConnectes.json ]
+then
+	touch UtilisateursConnectes.json
+	echo "{" >> UtilisateursConnectes.json
+	echo "	\"info_nombre_utilisateurs\":[" >> UtilisateursConnectes.json
+	echo "		{\"date\":\"`date`\", \"nombre_utilisateurs\":${UtilisateursNb}}," >> UtilisateursConnectes.json
+	echo "	]" >> UtilisateursConnectes.json
+	echo "}" >> UtilisateursConnectes.json
+else
+	echo "`head -n -2 UtilisateursConnectes.json`" > UtilisateursConnectes.json
+	echo "		{\"date\":\"`date`\", \"nombre_utilisateurs\":${UtilisateursNb}}," >> UtilisateursConnectes.json
+	echo "	]" >> UtilisateursConnectes.json
+	echo "}" >> UtilisateursConnectes.json
+fi
--- nombreProcessus.sh
+++ nombreProcessus.sh
@@ -2,4 +2,22 @@
 
 ProcessNb=`ps -aux | wc -l`
 
-echo "Nombre de processus : $ProcessNb"
+#echo "Nombre de processus : $ProcessNb"
+
+if [ ! -f nombreProcessus.json ]
+then
+	touch nombreProcessus.json
+	echo "{" >> nombreProcessus.json
+	echo "	\"infos_processus\":[" >> nombreProcessus.json
+	echo "		{\"date\":\"`date`\", \"nombre_processus\":${ProcessNb}}," >> nombreProcessus.json
+	echo "	]" >> nombreProcessus.json
+	echo "}" >> nombreProcessus.json
+else
+	echo "`head -n -2 nombreProcessus.json`" > nombreProcessus.json
+	echo "		{\"date\":\"`date`\", \"nombre_processus\":${ProcessNb}}," >> nombreProcessus.json
+	echo "	]" >> nombreProcessus.json
+	echo "}" >> nombreProcessus.json
+fi
+
+
+
